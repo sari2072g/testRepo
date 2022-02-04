df9cdcff42a8868057b9a52d90919ca59393315f
sarigutman <sarig2072@gmail.com>
2022-02-04 13:15:10 +0200

    add imprt

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
